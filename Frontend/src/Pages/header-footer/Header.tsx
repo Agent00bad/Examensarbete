ed2a1e6d4309c451eb84bd5d61485c54baea0198
Daniel <leotic.alden@gmail.com>
2024-05-21 16:48:13 +0200

    fixed react routing navigation

--- Frontend/src/Pages/header-footer/Header.tsx
+++ Frontend/src/Pages/header-footer/Header.tsx
@@ -1,32 +1,36 @@
 import "../../style/header.scss"
 import NavObject from "../../Interfaces/NavigationObject"
-
+import { NavLink } from "react-router-dom";
 const Header = () => {
 
 
-    const links : Array<NavObject> = [
-        {Path : "/", Name: "Home"}, 
-        {Path : "/experience", Name: "Experience"},
-        {Path : "/education", Name: "Education"},
-        {Path : "/projects", Name: "Projects"},
-        {Path : "/About", Name: "About"}
+    const links: Array<NavObject> = [
+        { Path: "/", Name: "Home" },
+        { Path: "/experience", Name: "Experience" },
+        { Path: "/education", Name: "Education" },
+        { Path: "/projects", Name: "Projects" },
+        { Path: "/About", Name: "About" }
     ]
 
-    return(
+    return (
         <div className="header">
             <nav className="navbar">
                 <div className="nav-div">
                     <ul className="nav-links">
-                        {links.map((nav, index) => {return(
-                            <li className={"link link-" + {index}} key={index}>
-                                <a href={nav.Path}>{nav.Icon != undefined 
-                                ? nav.Icon.ExternalUri
-                                // TODO: better implementation of external icon
-                                ? nav.Icon.Uri 
-                                : <img src={nav.Icon.Uri} alt={nav.Name + " icon"}/>
-                                : ""}{nav.Name}</a>
-                            </li>
-                        )})}
+                        {links.map((nav, index) => {
+                            return (
+                                <NavLink to={nav.Path}>
+                                    <li className={"link link-" + { index }} key={index}>
+                                        {nav.Icon != undefined
+                                            ? nav.Icon.ExternalUri
+                                                // TODO: better implementation of external icon
+                                                ? nav.Icon.Uri
+                                                : <img src={nav.Icon.Uri} alt={nav.Name + " icon"} />
+                                            : ""}{nav.Name}
+                                    </li>
+                                </NavLink>
+                            )
+                        })}
                     </ul>
                 </div>
             </nav>
